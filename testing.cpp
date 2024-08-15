4e4945533908cb290d155d84e8e913947f6361b1
RakibRHGit <rakibulhassan.himel@gamil.com>
2024-08-15 23:53:00 +0600

    new file

---
+++ testing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Initializing variables.
+    int a = 1690;
+    int b = 7;
+    int c = 20;
+    int d = 5;
+
+    // Performing division operations in C++.
+    int result = a / b;
+    int result2 = c / d;
+
+    // Displaying Results.
+    cout << a << " / " << b << " = " << result << endl;
+    cout << c << " / " << d << " = " << result2 << endl;
+}
